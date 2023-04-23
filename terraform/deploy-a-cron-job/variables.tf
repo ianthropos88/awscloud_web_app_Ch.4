4c3787e3042d1d4c491a01b12c2b3b0c6427d416
Partho CHOUDHURY <76818972+ianthropos88@users.noreply.github.com>
2023-04-23 19:32:44 +0200

    Create variables.tf

---
+++ terraform/deploy-a-cron-job/variables.tf
@@ -0,0 +1,15 @@
+variable "droplets_organization_id" {
+  type    = string
+}
+
+variable "droplets_access_token" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
