04c1d5e5c4c78e40112a50080ab01fd9cae052d5
Partho CHOUDHURY <76818972+ianthropos88@users.noreply.github.com>
2023-04-23 20:04:42 +0200

    Create variables.tf

---
+++ terraform/deploy-medusa-with-postgresql-and-redis-on-aws/variables.tf
@@ -0,0 +1,25 @@
+variable "droplets_organization_id" {
+  type = string
+}
+
+variable "droplets_access_token" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+variable "medusa_jwt_secret" {
+  type    = string
+  default = "your-super-secret" # TO CHANGE FOR PRODUCTION
+}
+
+variable "medusa_cookie_secret" {
+  type    = string
+  default = "your-super-secret-pt2" # TO CHANGE FOR PRODUCTION
+}
